9fc06994ed9502d17464aa8ff6f72c8c7f56c5e5
Abhay Sahani <abhay.cs.777@gmail.com>
2024-10-20 20:30:33 +0530

    scrap and temp.json

--- scrap.py
+++ scrap.py
@@ -13,10 +13,6 @@ def get_soup(url, header):
         urllib.request.Request(url, headers=header)),
         'html.parser')
 
-query = ["+pokemon+wallpaper", "+pokemon+card", "+pokemon+art"]
-
-header = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-
 def find_image(name, orname):
     url = "http://www.bing.com/images/search?q=" + name + "&FORM=HDRSC2"
     soup = get_soup(url, header)
@@ -33,28 +29,30 @@ def find_image(name, orname):
             pass
     return found_imgs
 
-with open("temp1.json") as file:
+query = ["+fruit", "+vegetable"]
+
+header = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+
+with open("temp.json") as file:
     dex = json.load(file)
     for key in dex:
         name = dex[key]["name"]
         print()
-        count = 2
+        count = 1
         for suffix in query:
             print(name + suffix, key)
-            c = 1
             found_imgs = find_image(name + suffix, name.lower())
             for link in found_imgs:
                 try:
                     img = Image.open(requests.get(link[0], stream=True).raw)
                     img = img.resize((224, 224))
-                    directory = f"E:/DataSet/{key}_{name}"
+                    directory = f"E:/DataSet/{name}"
                     if not os.path.exists(directory):
                         os.makedirs(directory)
-                    img.save(f"{directory}/{count}.{link[1]}")
+                    img.save(f"{directory}/image_{count}.{link[1]}")
                     count += 1
-                    c += 1
                 except Exception as e:
                     pass
-                if c == 20:
+                if count > 20:
                     break
         print()
